f6810fb38edfe8ba376e1481ce26c3f69647c386
Clara-Manzano <clara.manzano.corona@gmail.com>
2025-02-21 11:29:29 +0100

    UPDATED

--- src/app/mapa/RouteMap.tsx
+++ src/app/mapa/RouteMap.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet'
 import L, { LatLngExpression } from 'leaflet'
